03c16aab1f18084d45c94b47a347986e3ef323d9
StrikerRUS <nekit94-12@hotmail.com>
2018-09-29 23:38:28 +0300

    test

--- python-package/rgf/sklearn.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-__all__ = ('RGFClassifier', 'RGFRegressor',
-           'FastRGFClassifier', 'FastRGFRegressor')
-
-from rgf.rgf_model import RGFRegressor, RGFClassifier
-from rgf.fastrgf_model import FastRGFRegressor, FastRGFClassifier
