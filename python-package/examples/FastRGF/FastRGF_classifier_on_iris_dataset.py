66a6ad3cdaa227730b6e95c38b31fe17cf753684
Nikita Titov <nekit94-08@mail.ru>
2018-10-10 13:43:18 +0300

    Update FastRGF_classifier_on_iris_dataset.py

--- python-package/examples/FastRGF/FastRGF_classifier_on_iris_dataset.py
+++ python-package/examples/FastRGF/FastRGF_classifier_on_iris_dataset.py
@@ -1,9 +1,6 @@
-import time
-
 from sklearn import datasets
 from sklearn.utils.validation import check_random_state
-from sklearn.ensemble import GradientBoostingClassifier
-from rgf.sklearn import RGFClassifier, FastRGFClassifier
+from rgf.sklearn import RGFClassifier
 
 iris = datasets.load_iris()
 rng = check_random_state(0)
@@ -11,10 +8,6 @@ perm = rng.permutation(iris.target.size)
 iris.data = iris.data[perm]
 iris.target = iris.target[perm]
 
-start = time.time()
 clf = RGFClassifier()
 clf.fit(iris.data, iris.target)
-score = clf.score(iris.data, iris.target)
-end = time.time()
-print("RGF: {} sec".format(end - start))
-print("score: {}".format(score))
+#score = clf.score(iris.data, iris.target)
