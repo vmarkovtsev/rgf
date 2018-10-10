fb6326fc0d084baa1849896f19835a10a3c7451e
Nikita Titov <nekit94-12@hotmail.com>
2018-10-11 01:18:46 +0300

    Update FastRGF_classifier_on_iris_dataset.py

--- python-package/examples/FastRGF/FastRGF_classifier_on_iris_dataset.py
+++ python-package/examples/FastRGF/FastRGF_classifier_on_iris_dataset.py
@@ -1,12 +1,7 @@
 from sklearn import datasets
-from sklearn.utils.validation import check_random_state
 from rgf.sklearn import RGFClassifier
 
 iris = datasets.load_iris()
-rng = check_random_state(0)
-perm = rng.permutation(iris.target.size)
-iris.data = iris.data[perm]
-iris.target = iris.target[perm]
 
 clf = RGFClassifier()
 clf.fit(iris.data, iris.target)
